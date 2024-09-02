d7a88290c2a20b2f812ac1eb84a1660b6eae3c4d
x-star <qqshow@live.com>
2024-09-02 18:09:58 +0800

    add golang test client

--- src/test_client.go
+++ src/test_client.go
@@ -1,1 +1,28 @@
- 
+package main
+
+/*
+#cgo LDFLAGS: -L. -lclient
+#include <stdlib.h>
+
+void initialize();
+char* send_data(const char* data, int len);
+void free_data(char* data);
+*/
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	C.initialize()
+
+	data := "Hello, Server!"
+	cdata := C.CString(data)
+	defer C.free(unsafe.Pointer(cdata))
+
+	response := C.send_data(cdata, C.int(len(data)))
+	defer C.free_data(response)
+
+	fmt.Println(C.GoString(response))
+}
